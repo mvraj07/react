dbc1f8c6afc810a9df2ca764e130677393510a43
Raj Mahajan <mvraj7898@gmail.com>
2026-02-05 16:52:15 +0530

    update

--- todo-App/src/components/Searchbar.jsx
+++ todo-App/src/components/Searchbar.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import Button from "./common/Button";
-export default function SearchBar({ color, text }) {
+export default function SearchBar({ addTodo }) {
+  const [todo, setTodo] = useState("");
+  const [date, setDate] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!todo || !date) return;
+    addTodo({ text: todo, date });
+  };
+
   return (
     <>
-      <form action="#" method="post">
+      <form onSubmit={handleSubmit}>
         <div className="Searchbarcontainer">
-          <input type="text" />
-          <input type="date" />
-          <Button type="submit" color={color} text={text} />
+          <input
+            type="text"
+            placeholder="Todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+          <Button type="submit" color="green" text="Add ToDo" />
         </div>
       </form>
     </>
